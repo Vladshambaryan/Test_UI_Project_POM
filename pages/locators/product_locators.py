c8b9d9b618c79e9a481ec99fa35feb654ad594ef
PC <skachat30913@gmail.com>
2024-09-09 23:29:37 +0400

    Test_ui

--- pages/locators/product_locators.py
+++ pages/locators/product_locators.py
@@ -9,9 +9,7 @@ add_to_cart_loc = (By.XPATH, "//button[@type='submit'][contains(.,'Add to Cart')
 color_error_loc = (By.XPATH, "//div[@id='super_attribute[143]-error']")
 size_error_loc = (By.XPATH, "//div[@id='super_attribute[93]-error']")
 products1_loc = (By.CSS_SELECTOR, "li.product-item")
-# reviews_loc = (By.XPATH, "//span[contains(.,'Reviews')]")
-# add_reviews_loc = (By.XPATH, "//a[@class='action add']")
-# wish_list_loc = (By.XPATH, "(//span[contains(.,'Add to Wish List')])[1]")
+
 compare_loc = (By.XPATH, "(//span[contains(.,'Add to Compare')])[1]")
 comparision_list_loc = (By.XPATH, "(//div[contains(.,'You added product Ana Running Short to the comparison list.')])[6]")
 
