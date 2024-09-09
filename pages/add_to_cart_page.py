9cafe50898accf23d0eeb4cfdf1be4a98266b62e
PC <skachat30913@gmail.com>
2024-09-09 10:22:15 +0400

    Test_ui

--- pages/add_to_cart_page.py
+++ pages/add_to_cart_page.py
@@ -17,37 +17,21 @@ class AddToCart(BasePage):
         WebDriverWait(self.driver, 15).until(
             EC.text_to_be_present_in_element((By.XPATH, "//span[@class='counter-number']"), '1'))
 
-    def check_count(self):
+    def check_count(self, text):
         count = self.find(loc.count_loc)
         count.click()
-        assert count.text == '1'
+        assert count.text == text
 
-    def check_product_in_cart(self):
+    def check_product_in_cart(self, text):
         item_in_cart = WebDriverWait(self.driver, 15).until(
             EC.presence_of_element_located((By.XPATH, "//a[contains(.,'Ana Running Short')]")))
-        assert item_in_cart.text == 'Ana Running Short'
+        assert item_in_cart.text == text
 
-    def check_add_to_cart_with_out_selected_size_color(self):
+    def check_add_to_cart_with_out_selected_size_color(self, text):
         add_cart = self.find(loc.add_to_cart_loc)
         add_cart.click()
         color_error = self.find(loc.color_error_loc)
-        assert color_error.text == 'This is a required field.'
+        assert color_error.text == text
         size_error = self.find(loc.size_error_loc)
-        assert size_error.text == 'This is a required field.'
-
-
-# def select_product(self):
-#     short = self.find(ecoloc.short_loc)
-#     short.click()
-#     size = self.find(ecoloc.size_loc)
-#     size.click()
-#     color = self.find(ecoloc.color_loc)
-#     color.click()
-#     add_to_cart = self.find(ecoloc.add_to_cart_loc)
-#     add_to_cart.click()
-#
-# def check_product_in_cart(self, text):
-#     short1 = self.find(ecoloc.short1_loc)
-#     assert short1.text == text
-
+        assert size_error.text == text
 
--- tests/test_add_to_cart.py
+++ tests/test_add_to_cart.py
@@ -5,11 +5,11 @@ import pytest
 def test_add_to_cart(add_to_cart_page):
     add_to_cart_page.open_add_to_cart_page()
     add_to_cart_page.add_product_to_cart()
-    add_to_cart_page.check_count()
-    add_to_cart_page.check_product_in_cart()
+    add_to_cart_page.check_count('1')
+    add_to_cart_page.check_product_in_cart('Ana Running Short')
 
 
 @pytest.mark.regression
 def test_check_add_to_cart_with_out_selected_size_color(add_to_cart_page):
     add_to_cart_page.open_add_to_cart_page()
-    add_to_cart_page.check_add_to_cart_with_out_selected_size_color()
+    add_to_cart_page.check_add_to_cart_with_out_selected_size_color('This is a required field.')
